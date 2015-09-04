3cd64d4cf3f1433fa4d08e19201dcb9994ac3f8f
Omar Shaikh <oshaikh13@gmail.com>
2015-09-04 11:58:55 -0700

    Intergrated one time facebook login

--- www/js/login/login.js
+++ www/js/login/login.js
@@ -2,16 +2,24 @@
 
 angular.module('ionicApp.login', [])
 
-.controller('LoginCtrl', function($scope, $state, $ionicSlideBoxDelegate, $rootScope) {
+.controller('LoginCtrl', function($scope, $state, $ionicSlideBoxDelegate, $rootScope, $cordovaOauth, $localStorage) {
   $rootScope.login = true;
 
   // Called to navigate to the main app
   $scope.startApp = function() {
     $state.go('main');
   };
+
   $scope.loggingInFb = function() {
-    $state.go('selectActivity');
+    $cordovaOauth.facebook("840774716036629", ["email", "user_website", "user_location", "user_relationships"]).then(function(result) {
+        $localStorage.access_token = result.access_token;
+        $state.go("selectActivity");
+    }, function(error) {
+        alert("There was a problem signing in!  See the console for logs");
+        console.log(error);
+    });
   };
+
   $scope.loggingInTwitter = function() {
     $state.go('selectActivity');
   };
