5f804a5533f2c6a800eb41bf0191545710f060f7
Omar Shaikh <oshaikh13@gmail.com>
2015-09-03 17:17:10 -0700

    Fixed scroll to bottom

--- www/js/chat/chat.js
+++ www/js/chat/chat.js
@@ -6,12 +6,13 @@ angular.module('ionicApp.chat', [])
   $scope.chats = [{name: "Omar", lastText: "LOL", face: "http://img.timeinc.net/time/daily/2010/1011/poy_nomination_agassi.jpg", id: 123}];
 })
 
-.controller('chatDetailCtrl', function($scope, $state, $rootScope) {
+.controller('chatDetailCtrl', function($scope, $state, $rootScope, $ionicScrollDelegate) {
   // some get req based on the id to get info on that certian chat
   // each chat room has a socket id.... we can use that id itself... bam
 
   // ajax request here
   // ajax response here 
+  var viewScroll = $ionicScrollDelegate.$getByHandle('userMessageScroll');
   $scope.user = {_id: 123};
   $scope.chatMsgs = [{text: 'hi', userId: 1234}, {text: 'hi', userId: 123}, {text: 'hi', userId: 1234}, {text: 'hi', userId: 123}];
 
@@ -22,6 +23,8 @@ angular.module('ionicApp.chat', [])
   $scope.submitText = function(text){
     $scope.chatMsgs.push({text: text, userId: 123, time: new Date()});
     $scope.input.message = "";
+    viewScroll.resize();
+    viewScroll.scrollBottom();
   }
 })
 
