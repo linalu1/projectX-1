4e4a5248b0bc2e427f4d17a9597e58c067a9c2ba
Lina Lu <linalu001@gmail.com>
2015-09-04 19:59:49 -0700

    omar test

--- www/js/app.js
+++ www/js/app.js
@@ -20,46 +20,45 @@ angular.module('ionicApp', [
     url: '/',
     templateUrl: 'js/login/login.html',
     controller: 'LoginCtrl',
-    authenticate: false
   })
   .state('addFbLikes', {
     url: '/addFbLikes',
     templateUrl: 'js/addFbLikes/addFbLikes.html',
     controller: 'addFbLikesCtrl',
-    authenticate: false // change to false for testing
+    // authenticate: true // change to 'false' for testing
   })
   .state('chat', {
     url: '/chat',
     templateUrl: 'js/chat/chat.html',
     controller: 'chatCtrl',
-    authenticate: false // change to false for testing
+    // authenticate: true // change to 'false' for testing
 
   })
   .state('chat-detail', {
     url: '/chat/:chatId',
     templateUrl: 'js/chat/chatdetail.html',
     controller: 'chatDetailCtrl',
-    authenticate: false // change to false for testing
+    // authenticate: true // change to 'false' for testing
 
   })
   .state('home', {
     url: '/home',
     templateUrl: 'js/otherUsers/otherUsers.html',
     controller: 'otherUsersCtrl',
-    authenticate: false // change to false for testing
+    // authenticate: true // change to 'false' for testing
 
   })
   .state('profile', {
     url: '/profile',
     templateUrl: 'js/profile/profile.html',
     controller: 'profileCtrl',
-    authenticate: false // change to false for testing
+    // authenticate: true // change to 'false' for testing
   })
   .state('selectActivity', {
     url: '/selectActivity',
     templateUrl: 'js/selectActivity/selectActivity.html',
     controller: 'selectActivityCtrl',
-    authenticate: false // change to false for testing
+    // authenticate: true // change to 'false' for testing
 
   });
   $urlRouterProvider.otherwise("/");
