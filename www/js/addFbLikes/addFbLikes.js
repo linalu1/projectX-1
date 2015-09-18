84c55cdc524876dfc03c4e7f3027faaed562b306
Lina Lu <linalu001@gmail.com>
2015-09-18 13:38:53 -0800

    [build/chat] comment in fblikes

--- www/js/addFbLikes/addFbLikes.js
+++ www/js/addFbLikes/addFbLikes.js
@@ -3,8 +3,9 @@
 angular.module('ionicApp.addFbLikes', [])
 
 .controller('addFbLikesCtrl', function($scope, $state, $rootScope) {
+
+  // set login rootScope variable to false to display bottom tab bar. (Since it's not the login screen);
   $rootScope.login = false;
-  $rootScope.isPrivateChat = false;
 
 });
 
